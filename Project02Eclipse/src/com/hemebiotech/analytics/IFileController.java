ce572d2c6bb894a54258bc0845fccd39ae18867c
sleabykhalil <sleabykhalil@users.noreply.github.com>
2020-07-24 18:20:16 +0200

    Add SystemService class and ISystemService Change name to FileController and IFileController Use Functions in main

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/IFileController.java
@@ -0,0 +1,37 @@
+package com.hemebiotech.analytics;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Anything that will read symptom data from a source
+ * The important part is, the return value from the operation, which is a list of strings,
+ * that may contain many duplications
+ * 
+ * The implementation does not need to order the list
+ * 
+ */
+public interface IFileController {
+
+
+	/**
+	 * If no data is available, return an empty List
+	 * This will read from input File,
+	 * 
+	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 */
+	List<String> ReadSymptoms ();
+
+
+
+	/**
+	 * This function to write the results in file
+	 * @param symptomsMap Symptoms counted and they will be ordered as they are in TreeMap
+	 * @throws IOException
+	 */
+	void WriteSymptoms(TreeMap<String,Integer> symptomsMap) throws IOException;
+
+
+}
