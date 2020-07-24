34deef811eb5a09d85c08fa049341ece786d7dd2
sleabykhalil <sleabykhalil@users.noreply.github.com>
2020-07-24 11:48:23 +0200

    2em vr    Comment old coding    TreeMap for sorting

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -16,11 +16,26 @@ import java.util.TreeMap;
 public interface ISymptomReader {
 	/**
 	 * If no data is available, return an empty List
+	 * This will read from input File,
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-
 	List<String> GetSymptoms ();
-	Map<String,Symptom> CountSymptoms(List<String> Symptoms);
+
+	/**
+	 * This tacks items from input string list and put the items in Map the
+	 * 														key is Symptom value is the counter
+	 * @param Symptoms list returns from GetSymptoms function
+	 * 
+	 * @return Map contains all items without duplicates as keys and how many times they appears as values
+	 */
+	Map<String,Integer> CountSymptoms(List<String> Symptoms);
+
+	/**
+	 * This function to write the results in file
+	 * @param fileName file path to write result in it
+	 * @param symptomsMap Symptoms counted and they will be ordered as they are in TreeMap
+	 * @throws IOException
+	 */
 	void WriteSymptoms(String fileName,TreeMap<String,Integer> symptomsMap) throws IOException;
 }
