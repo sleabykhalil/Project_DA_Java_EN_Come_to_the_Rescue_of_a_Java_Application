0a9ef4ce4d6d6ad94911d300d4290175685e334f
sleabykhalil <sleabykhalil@users.noreply.github.com>
2020-08-03 00:39:57 +0200

    move defaultProperties to FileController

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,27 +1,16 @@
 package com.hemebiotech.analytics;
 
 
-import java.io.FileInputStream;
-import java.util.Properties;
 
 public class AnalyticsCounter {
 
 
-	public static void main(String args[]) throws Exception {
-
-		Properties defaultProps = new Properties();
-		FileInputStream in = new FileInputStream("defaultProperties");
-		defaultProps.load(in);
-		in.close();
-		defaultProps.get("source");
-		defaultProps.get("destination");
+	public static void main(String args[])  {
 
 
 		FileController fileController = new FileController();
 		SystemServices systemServices = new SystemServices();
 
-		fileController.setSourceFilepath((String) defaultProps.get("source"));
-		fileController.setDestinationFilepath((String) defaultProps.get("destination"));
 
 		fileController.WriteSymptoms(systemServices.CountSymptoms(fileController.ReadSymptoms()));
 
