ca4e7a1e90a019f0092f30f9b30c5befa227cec0
sleabykhalil <sleabykhalil@users.noreply.github.com>
2020-08-02 23:37:33 +0200

    Add Default Properties

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,17 +1,27 @@
 package com.hemebiotech.analytics;
 
 
+import java.io.FileInputStream;
+import java.util.Properties;
 
 public class AnalyticsCounter {
 
 
 	public static void main(String args[]) throws Exception {
 
+		Properties defaultProps = new Properties();
+		FileInputStream in = new FileInputStream("defaultProperties");
+		defaultProps.load(in);
+		in.close();
+		defaultProps.get("source");
+		defaultProps.get("destination");
+
+
 		FileController fileController = new FileController();
 		SystemServices systemServices = new SystemServices();
 
-		fileController.setSourceFilepath("Project02Eclipse/symptoms.txt");
-		fileController.setDestinationFilepath("result.out");
+		fileController.setSourceFilepath((String) defaultProps.get("source"));
+		fileController.setDestinationFilepath((String) defaultProps.get("destination"));
 
 		fileController.WriteSymptoms(systemServices.CountSymptoms(fileController.ReadSymptoms()));
 
